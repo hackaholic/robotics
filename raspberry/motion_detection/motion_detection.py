dd66245d0d68008bd6a19b789bcbbe6510e472b2
hackaholic <juniourkawasakicool@gmail.com>
2017-02-13 09:42:45 +0000

    added motion_detection.py

---
+++ raspberry/motion_detection/motion_detection.py
@@ -0,0 +1,23 @@
+import sys
+import time
+import RPi.GPIO as gpio
+
+gpio.setwarnings(False)
+gpio.setmode(gpio.BOARD)
+gpio.setup(7,gpio.IN)
+
+def motion_detect():
+    try:
+        while True:
+            if gpio.input(7)==1:
+                print "Motion Detected"
+    except KeyboardInterrupt:
+        gpio.cleanup()
+        print "\n Terminated by User"
+        sys.exit()
+
+
+if __name__ == "__main__":
+    motion_detect()
+
+
